fba49bde262f566de0fa90c466159953b5a42338
gdpreethi <gd.preethi@gmail.com>
2025-02-17 20:25:13 +0530

    Update HelloTestLeaf.java

--- week1/day1/HelloTestLeaf.java
+++ week1/day1/HelloTestLeaf.java
@@ -3,6 +3,7 @@ package week1.day1;
 public class HelloTestLeaf {
 
 	public static void main(String[] args) {
+		//Displaying the text "Welcome to TestLeaf" in console
 		System.out.println("Welcome to Testleaf");
 	}
 }
